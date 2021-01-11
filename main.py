49cd71b1711f9ca7433d72eef120391cbc00737f
Awank Kaley <38899883+awankkaley@users.noreply.github.com>
2021-01-11 23:18:58 +0700

    ok

--- main.py
+++ main.py
@@ -126,6 +126,5 @@ def get_data():
     else:
         return {'code': 400, 'msg': 'Data Tidak Ditemukan'}, 200
 
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', threaded=True, port=5004)
